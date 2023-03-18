ac25663d070859e2099ae67bdaf4f75a6ae19117
Ilya Buglakov <ilboogl@gmail.com>
2023-03-18 15:11:46 +0300

    chore(build.gradle.kts): remove spring data mongo db for now

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,13 +20,9 @@ val embeddedMongoVersion by properties
 val snippetsDir = file(property("snippetsDirPath")!!)
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
-
-	testImplementation("de.flapdoodle.embed:de.flapdoodle.embed.mongo:$embeddedMongoVersion")
-	testImplementation("de.flapdoodle.embed:de.flapdoodle.embed.mongo.spring30x:$embeddedMongoVersion")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
